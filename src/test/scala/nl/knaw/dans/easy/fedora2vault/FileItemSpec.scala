e10cb44ff51f4f46ff929d9c6d54df85ba27e192
jo-pol <jo-pol@users.noreply.github.com>
2020-06-26 07:59:28 +0200

    test driven sample

--- src/test/scala/nl/knaw/dans/easy/fedora2vault/FileItemSpec.scala
+++ src/test/scala/nl/knaw/dans/easy/fedora2vault/FileItemSpec.scala
@@ -43,6 +43,18 @@ class FileItemSpec extends TestSupportFixture {
     )
   }
 
+  it should "report a missing tag (this time no default for accessibleTo)" in {
+    val fileMetadata = <name>something.txt</name>
+                       <path>original/something.txt</path>
+                       <mimeType>text/plain</mimeType>
+                       <visibleTo>ANONYMOUS</visibleTo>
+
+    FileItem(fileFoXml(fileMetadata)) should matchPattern {
+      case Failure(e: Exception) if e.getMessage ==
+        "No <accessibleTo> in EASY_FILE_METADATA for easy-file:35" =>
+    }
+  }
+
   it should "report a repeated tag" in {
     val fileMetadata = <name>something.txt</name>
                        <path>original/something.txt</path>
@@ -55,15 +67,50 @@ class FileItemSpec extends TestSupportFixture {
     }
   }
 
-  it should "report a missing tag" in {
-    val fileMetadata = <name>something.txt</name>
-                       <path>original/something.txt</path>
-                       <visibleTo>NONE</visibleTo>
 
-    FileItem(fileFoXml(fileMetadata)) should matchPattern {
-      case Failure(e: Exception) if e.getMessage ==
-        "No <mimeType> in EASY_FILE_METADATA for easy-file:35" =>
-    }
+  it should "do additional metadata" in {
+    val fileMetadata =
+      <name>SKKJ6_spoor.mif</name>
+      <path>GIS/SKKJ6_spoor.mif</path>
+      <mimeType>application/x-framemaker</mimeType>
+      <size>911988</size>
+      <creatorRole>ARCHIVIST</creatorRole>
+      <visibleTo>ANONYMOUS</visibleTo>
+      <accessibleTo>KNOWN</accessibleTo>
+      <addmd:additional-metadata>
+        <properties></properties>
+        <addmd:additional id="addi" label="archaeology-filemetadata">
+          <content>
+            <file_category>GIS</file_category>
+            <original_file>Skkj6_spoor.TAB</original_file>
+            <file_content>Alle sporenkwaart</file_content>
+            <file_name>SKKJ6_spoor.mif</file_name>
+            <file_required>SKKJ6_spoor.mid</file_required>
+            <software>MapInfo</software>
+            <analytic_units>antropogene en natuurlijke sporen</analytic_units>
+            <mapprojection>non-earth (in m.), met de waarden van het RD-stelsel</mapprojection>
+            <notes>alle sporen samen vormen de putomtrek</notes>
+          </content>
+        </addmd:additional>
+      </addmd:additional-metadata>
+
+    FileItem(fileFoXml(fileMetadata)) shouldBe Success(
+    <file filepath="data/GIS/SKKJ6_spoor.mif">
+      <dcterms:identifier>easy-file:35</dcterms:identifier>
+      <dcterms:title>SKKJ6_spoor.mif</dcterms:title>
+      <dcterms:format>application/x-framemaker</dcterms:format>
+      <accessibleToRights>KNOWN</accessibleToRights>
+      <visibleToRights>ANONYMOUS</visibleToRights>
+      <zz>GIS</zz>
+      <dcterms:isFormatOf>Skkj6_spoor.TAB</dcterms:isFormatOf>
+      <dcterms:abstract>Alle sporenkwaart</dcterms:abstract>
+      <dcterms:requires>SKKJ6_spoor.mid</dcterms:requires>
+      <dcterms:description>This file was created with MapInfo</dcterms:description>
+      <xx>antropogene en natuurlijke sporen</xx>
+      <yy>non-earth (in m.), met de waarden van het RD-stelsel</yy>
+      <dcterms:description>alle sporen samen vormen de putomtrek</dcterms:description>
+    </file>
+    )
   }
 
   private def fileFoXml(fileMetadata: NodeBuffer) = {
@@ -74,7 +121,7 @@ class FileItemSpec extends TestSupportFixture {
           <foxml:datastream ID="EASY_FILE_METADATA" STATE="A" CONTROL_GROUP="X" VERSIONABLE="false">
               <foxml:datastreamVersion ID="EASY_FILE_METADATA.0" LABEL="" CREATED="2020-03-17T10:24:17.660Z" MIMETYPE="text/xml" SIZE="359">
                   <foxml:xmlContent>
-                      <fimd:file-item-md xmlns:fimd="http://easy.dans.knaw.nl/easy/file-item-md/" version="0.1">
+                      <fimd:file-item-md xmlns:addmd="http://easy.dans.knaw.nl/easy/additional-metadata/" xmlns:fimd="http://easy.dans.knaw.nl/easy/file-item-md/" version="0.1">
                           { fileMetadata }
                       </fimd:file-item-md>
                   </foxml:xmlContent>
